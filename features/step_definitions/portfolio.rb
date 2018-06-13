a9a26ef98d74d155fef1d76f386abe034c9f712d
jiejidgray <jie.ji@utt.fr>
2018-06-13 09:50:46 +0200

    [TEST]update item's attribute v1

--- features/step_definitions/portfolio.rb
+++ features/step_definitions/portfolio.rb
@@ -18,16 +18,57 @@ end
 
 Soit("{string} le portfolio spécifié dans la configuration") do |portfolio|
   case portfolio
-  when "vitraux" 
-    true #current configuration
-  when "indéfini" 
+  when "vitraux"
+    true
+  when "indéfini"
     pending "alternate configuration"
   else
     false
   end
 end
 
-# Events 
+Soit("les informations de {string} stocké dans le serveur utt")do |nom|
+end
+
+Soit("le login de {string} qui est {string}")do |nom, login|
+end
+
+Soit("le mot de passe de {string} qui est {string}")do |nom, password|
+end
+
+
+Soit("un visiteur saisit {string} dans le champs login et {string} dans le champs mot de passe") do|login, password|
+  case login
+  when "jie.ji@utt.fr"
+    true
+  when "jj"
+    false
+  end
+  case password
+  when "passe"
+    true
+  else
+    false
+  end
+end
+
+Soit("l'utilisateur ayant un Item lui correspondant au portfolio {string}") do |portfolio|
+# On the remote servers
+end
+
+Soit("l'Attribute de son Item comprenant l'AttributeName{string}")do |attributeName|
+
+end
+
+Soit("l'AttributeName {string} étant avec l'AttributeValue {string}")do |attributeName, attributeValue|
+
+end
+
+Soit("l'utilisateur étant sur la page de modification de profil")do
+
+end
+
+# Events
 
 Quand("un visiteur ouvre la page d'accueil du site") do
   visit "/"
@@ -35,6 +76,18 @@ end
 
 Quand("un visiteur ouvre la page d‘accueil d‘un site dont l‘adresse commence par {string}") do |portfolio|
   visit "/"
+end
+
+Quand("il clique sur valider") do
+  visit "/"
+end
+
+Quand("il modifie l'AttributeName {string} avec l'AttributeValue {string}")do |attributeName, attributeValue|
+  visit "/"
+end
+
+Et("il clique sur le bouton {string}")do |bouton|
+ click "Enregistrer"
 end
 
 # Outcomes
@@ -51,3 +104,20 @@ Alors("un des corpus affichés est {string}") do |corpus|
   expect(page).to have_content corpus
 end
 
+Alors("le pop-up se ferme")do
+
+end
+
+Alors("l'AttributeValue de l'AttributeName {string} change de {string} à {string} dans la base de données") do |attributeName, attributeValue1,attributeValue2|
+  expect(page).to have_content attributeName
+  expect(page).to have_content attributeValue
+  expect(page).to have_content attributeValue2
+end
+
+Et("un message disant que la modification est bien enregistrée affiche")do
+end
+
+Et("sur la page profil de cet utilisateur l'AttributeName {string} est avec l'AttributeValue {string}")do |attributeName, attributeValue|
+  expect(page).to have_content(attributeName)
+  expect(page).to have_content(attributeValue)
+end
